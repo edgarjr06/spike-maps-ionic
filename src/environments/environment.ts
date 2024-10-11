72e3ebf9392b4adfe2677d02556006fad27953e5
JoseOtero15 <joseotero1504@gmail.com>
2024-10-11 17:54:09 -0500

    mapbox accestoken deleted

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  mapboxAccessToken: "pk.eyJ1IjoiZWRnYXItb3Rlcm8iLCJhIjoiY20yMjR4dXAzMDNkdTJrcHN2YmdiNmZ5NiJ9.2MDIZqOLzEt5aeeuCoaTiQ"
+  mapboxAccessToken: ""
 };
 
 /*
